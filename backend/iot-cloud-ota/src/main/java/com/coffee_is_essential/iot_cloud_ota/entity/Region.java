9363d556079823df25bc6a13ba72a02208818de4
hardjava <fkdlcn123@kumoh.ac.kr>
2025-07-09 18:19:47 +0900

    feat: region DB 세팅

--- backend/iot-cloud-ota/src/main/java/com/coffee_is_essential/iot_cloud_ota/entity/Region.java
+++ backend/iot-cloud-ota/src/main/java/com/coffee_is_essential/iot_cloud_ota/entity/Region.java
@@ -4,6 +4,9 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
+/**
+ * AWS 리전 정보를 데이터베이스에 저장하기 위한 엔티티 클래스 입니다.
+ */
 @Entity
 public class Region {
     @Id
