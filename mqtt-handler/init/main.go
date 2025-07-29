8d2a061f245cca329d75e695d479ade77f9a982d
hardjava <fkdlcn123@kumoh.ac.kr>
2025-07-29 22:28:52 +0900

    feat: initial setting

--- mqtt-handler/init/main.go
+++ mqtt-handler/init/main.go
@@ -5,7 +5,5 @@ import (
 )
 
 func main() {
-	//client := mqtt.NewClient("tcp://emqx-nlb-bfefab8efb8db52f.elb.ap-northeast-2.amazonaws.com:1883", "mqtt-handler")
-	//client.Publish("test/topic", 1, false, "test입니다.")
 	cmd.NewCmd("config.toml")
 }
