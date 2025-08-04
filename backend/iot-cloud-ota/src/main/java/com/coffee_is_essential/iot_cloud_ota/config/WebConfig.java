46bbc15cef9ba0063280da4460bcf6f24285e79a
Park Junwoo <boksam1017@gmail.com>
2025-08-05 08:53:37 +0900

    configure CORS

--- backend/iot-cloud-ota/src/main/java/com/coffee_is_essential/iot_cloud_ota/config/WebConfig.java
+++ backend/iot-cloud-ota/src/main/java/com/coffee_is_essential/iot_cloud_ota/config/WebConfig.java
@@ -1,15 +1,20 @@
 package com.coffee_is_essential.iot_cloud_ota.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${cors.allowed-origins}")
+    private String allowedOrigins;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://iot-cloud-ota-frontend-bucket.s3-website.ap-northeast-2.amazonaws.com")
+                .allowedOrigins(allowedOrigins.split(","))
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
