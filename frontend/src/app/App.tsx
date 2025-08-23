64e60580e0f655c98d5aff12ea15e2ebd5d22e21
Park Junwoo <boksam1017@gmail.com>
2025-08-23 17:11:54 +0900

    use react-query to refresh when data has been updated

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -2,10 +2,13 @@ import { RouterProvider } from "react-router";
 import { Router } from "./Router";
 import { css, Global } from "@emotion/react";
 import { ToastContainer } from "react-toastify";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div>
+    <QueryClientProvider client={queryClient}>
       <Global
         styles={css`
           * {
@@ -15,7 +18,7 @@ function App() {
       />
       <RouterProvider router={Router} />
       <ToastContainer />
-    </div>
+    </QueryClientProvider>
   );
 }
 
