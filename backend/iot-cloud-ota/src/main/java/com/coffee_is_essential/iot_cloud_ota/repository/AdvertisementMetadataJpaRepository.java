0bd91daf7c76e8238b1fd9e84ede03cb49bfaade
hardjava <fkdlcn123@kumoh.ac.kr>
2025-09-04 00:30:27 +0900

    feat/backend: 업로드된 광고 조회 api 구현

--- backend/iot-cloud-ota/src/main/java/com/coffee_is_essential/iot_cloud_ota/repository/AdvertisementMetadataJpaRepository.java
+++ backend/iot-cloud-ota/src/main/java/com/coffee_is_essential/iot_cloud_ota/repository/AdvertisementMetadataJpaRepository.java
@@ -1,7 +1,11 @@
 package com.coffee_is_essential.iot_cloud_ota.repository;
 
 import com.coffee_is_essential.iot_cloud_ota.entity.AdvertisementMetadata;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,4 +15,13 @@ public interface AdvertisementMetadataJpaRepository extends JpaRepository<Advert
     Optional<AdvertisementMetadata> findByTitle(String title);
 
     boolean existsByOriginalS3Path(String originalS3Path);
+
+    @Query("""
+            SELECT ads
+            FROM AdvertisementMetadata ads
+            WHERE (:keyword IS NULL OR :keyword = '' OR
+                   ads.title LIKE %:keyword% OR
+                   ads.description LIKE %:keyword%)
+            """)
+    Page<AdvertisementMetadata> searchWithNullableKeyword(@Param("keyword") String keyword, Pageable pageable);
 }
--- backend/iot-cloud-ota/src/main/java/com/coffee_is_essential/iot_cloud_ota/service/AdvertisementService.java
+++ backend/iot-cloud-ota/src/main/java/com/coffee_is_essential/iot_cloud_ota/service/AdvertisementService.java
@@ -1,15 +1,21 @@
 package com.coffee_is_essential.iot_cloud_ota.service;
 
+import com.coffee_is_essential.iot_cloud_ota.domain.PaginationInfo;
 import com.coffee_is_essential.iot_cloud_ota.domain.S3FileHashResult;
-import com.coffee_is_essential.iot_cloud_ota.dto.AdvertisementMetadataRequestDto;
-import com.coffee_is_essential.iot_cloud_ota.dto.AdvertisementMetadataResponseDto;
+import com.coffee_is_essential.iot_cloud_ota.dto.*;
 import com.coffee_is_essential.iot_cloud_ota.entity.AdvertisementMetadata;
 import com.coffee_is_essential.iot_cloud_ota.repository.AdvertisementMetadataJpaRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -29,7 +35,7 @@ public class AdvertisementService {
      * @return 저장된 광고 메타데이터 응답 DTO
      */
     @Transactional
-    public AdvertisementMetadataResponseDto saveAdvertisementMetadata(AdvertisementMetadataRequestDto requestDto) {
+    public SaveAdvertisementMetadataResponseDto saveAdvertisementMetadata(AdvertisementMetadataRequestDto requestDto) {
         if (advertisementMetadataJpaRepository.findByTitle(requestDto.title()).isPresent()) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, requestDto.title() + "의 광고 제목이 이미 존재합니다.");
         }
@@ -50,6 +56,38 @@ public class AdvertisementService {
 
         AdvertisementMetadata savedAdvertisementMetadata = advertisementMetadataJpaRepository.save(advertisementMetadata);
 
-        return AdvertisementMetadataResponseDto.from(savedAdvertisementMetadata);
+        return SaveAdvertisementMetadataResponseDto.from(savedAdvertisementMetadata);
+    }
+
+    /**
+     * 광고 메타데이터를 페이지네이션 방식으로 조회합니다.
+     * 검색어(keyword)가 주어지면 제목/설명에 대해 부분 검색 수행
+     * 최신순(createdAt 기준 내림차순)으로 정렬
+     * 조회된 엔티티를 응답 DTO로 변환하면서 Presigned 다운로드 URL 생성
+     * 페이지네이션 메타데이터(Page 번호, 크기, 전체 페이지 수, 전체 아이템 수) 생성
+     *
+     * @param paginationInfo 페이지 번호, 페이지 크기, 검색어 정보를 담은 DTO
+     * @return 광고 메타데이터 리스트와 페이지네이션 정보를 포함한 응답 DTO
+     */
+    public AdvertisementMetadataWithPageResponseDto findAllWithPagination(PaginationInfo paginationInfo) {
+        Pageable pageable = PageRequest.of(paginationInfo.page() - 1, paginationInfo.limit(), Sort.by("createdAt").descending());
+        String keyword = paginationInfo.search();
+
+        Page<AdvertisementMetadata> findAds = advertisementMetadataJpaRepository.searchWithNullableKeyword(
+                keyword, pageable
+        );
+
+        List<AdvertisementMetadataResponseDto> ads = findAds.getContent().stream()
+                .map(ad -> AdvertisementMetadataResponseDto.from(ad, s3Service.getAdsPresignedDownloadUrl(ad.getTitle()).url()))
+                .toList();
+
+        PaginationMetadataDto metadataDto = new PaginationMetadataDto(
+                findAds.getPageable().getPageNumber() + 1,
+                findAds.getPageable().getPageSize(),
+                findAds.getTotalPages(),
+                findAds.getTotalElements()
+        );
+
+        return new AdvertisementMetadataWithPageResponseDto(ads, metadataDto);
     }
 }
