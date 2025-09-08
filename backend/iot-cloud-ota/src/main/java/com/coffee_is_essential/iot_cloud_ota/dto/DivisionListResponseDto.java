e637357e6ade566a648386882ac8585eee10fd0d
hardjava <fkdlcn123@kumoh.ac.kr>
2025-09-08 22:47:10 +0900

    feat: 리전 리스트 조회 api 개발

--- backend/iot-cloud-ota/src/main/java/com/coffee_is_essential/iot_cloud_ota/dto/DivisionListResponseDto.java
+++ backend/iot-cloud-ota/src/main/java/com/coffee_is_essential/iot_cloud_ota/dto/DivisionListResponseDto.java
@@ -6,23 +6,17 @@ import org.springframework.data.domain.Page;
 import java.util.List;
 
 public record DivisionListResponseDto(
-        List<DivisionResponseDto> divisions,
+        List<DivisionResponseDto> items,
         PaginationMetadataDto paginationMeta
 ) {
     public static DivisionListResponseDto from(Page<Division> divisionPage) {
-        PaginationMetadataDto metadataDto = new PaginationMetadataDto(
-                divisionPage.getPageable().getPageNumber() + 1,
-                divisionPage.getPageable().getPageSize(),
-                divisionPage.getTotalPages(),
-                divisionPage.getTotalElements()
-        );
 
         return new DivisionListResponseDto(
                 divisionPage.getContent()
                         .stream()
                         .map(DivisionResponseDto::from)
                         .toList(),
-                metadataDto
+                PaginationMetadataDto.from(divisionPage)
         );
     }
 }
