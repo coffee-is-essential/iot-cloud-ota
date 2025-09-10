9a50f07f437c2c214cd25b2ce4446bf841e57a8a
Park Junwoo <boksam1017@gmail.com>
2025-09-10 21:36:36 +0900

    implement modal

---
+++ frontend/src/features/device_register/ui/DeviceRegisterForm.tsx
@@ -0,0 +1,11 @@
+import { JSX } from "react";
+
+export interface DeviceRegisterFormProps {
+  onClose?: () => void;
+}
+
+export const DeviceRegisterForm = ({
+  onClose,
+}: DeviceRegisterFormProps): JSX.Element => {
+  return <div>DeviceRegisterForm</div>;
+};
